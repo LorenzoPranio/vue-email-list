3dbf5c1f3e7c7483b9f7e79c7c56fe3c3d0f37d8
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-25 16:02:32 +0200

    [UPD]Created gebnerate random mail function in script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            random_mail: null;
+            random_mail: []
         }
     },
     methods: {
+        generate_random_mail(){
+            for (let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+                    this.random_mail = resp.data.response
+                });
+            }
+        },
     }
 }).mount('#app');
