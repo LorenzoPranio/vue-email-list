52ec5bbc5f3b39527e33851ad21ded6ed49f4f5f
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-25 16:26:50 +0200

    [UPD]Added "Created" for generate random mail in script.js

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@ createApp({
             random_mail: []
         }
     },
+    created(){
+        this.generate_random_mail();
+    },
     methods: {
         generate_random_mail(){
+            this.random_mail = []
             for (let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-                    this.random_mail = resp.data.response
+                    this.random_mail.push(resp.data.response)
                 });
             }
-        },
+        }
     }
 }).mount('#app');
